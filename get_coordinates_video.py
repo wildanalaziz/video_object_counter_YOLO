09b9d4e72853aefc6bcd1a0ac87c5427e8567f40
Achmad Wildan <wildanalaziz82@gmail.com>
2024-07-23 14:10:58 +0700

    tambah get_coordinates_video.py untuk memmudahkan menentukan kordinat counter region

---
+++ get_coordinates_video.py
@@ -0,0 +1,69 @@
+# importing the module 
+import cv2 
+   
+# function to display the coordinates of 
+# of the points clicked on the image  
+def click_event(event, x, y, flags, params): 
+  
+    # checking for left mouse clicks 
+    if event == cv2.EVENT_LBUTTONDOWN: 
+  
+        # displaying the coordinates 
+        # on the Shell 
+        print(x, ' ', y) 
+  
+        # displaying the coordinates 
+        # on the image window 
+        font = cv2.FONT_HERSHEY_SIMPLEX 
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x,y), font, 
+                    1, (255, 0, 0), 2) 
+        cv2.imshow('image', img) 
+  
+    # checking for right mouse clicks      
+    if event==cv2.EVENT_RBUTTONDOWN: 
+  
+        # displaying the coordinates 
+        # on the Shell 
+        print(x, ' ', y) 
+  
+        # displaying the coordinates 
+        # on the image window 
+        font = cv2.FONT_HERSHEY_SIMPLEX 
+        b = img[y, x, 0] 
+        g = img[y, x, 1] 
+        r = img[y, x, 2] 
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r), 
+                    (x,y), font, 1, 
+                    (255, 255, 0), 2) 
+        cv2.imshow('image', img) 
+  
+# driver function 
+if __name__=="__main__": 
+  
+    # reading the image 
+    source = "rtsp://user:password@127.0.0.1:22/H.264"
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL) 
+    frame_w, frame_h = 1280,720
+
+    VideoCapture = cv2.VideoCapture(source)
+    while VideoCapture.isOpened():
+        sucess, img = VideoCapture.read()
+        if not sucess:
+            break
+  
+        # displaying the image 
+        img = cv2.resize(img, (frame_w, frame_h))
+        cv2.imshow('image', img) 
+    
+        # setting mouse handler for the image 
+        # and calling the click_event() function 
+        cv2.setMouseCallback('image', click_event) 
+    
+        # wait for a key to be pressed to exit 
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+  
+    # close the window 
+    cv2.destroyAllWindows() 
